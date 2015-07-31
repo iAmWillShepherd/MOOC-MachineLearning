42448deb405764f4f102dcf7229fd1e5f87c89a1
William Shepherd <williamrshepherd@gmail.com>
2015-07-31 13:49:34 -0500

    Implemented regularized gradient function

--- Machine Learning/machine-learning-ex2/ex2/costFunctionReg.m
+++ Machine Learning/machine-learning-ex2/ex2/costFunctionReg.m
@@ -17,20 +17,24 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
-thetaOneToN = theta(2:length(theta));
 
+% Get Theta 0 and Theta 1 -> n
+lengthOfTheta = length(theta);
+theta_0 = theta(1);
+theta_N = theta(2:length(theta));
+
+% Compute cost J
 positiveClass = -y' * log(sigmoid(X * theta));
 negativeClass = (1 - y)' * log(1 - sigmoid(X * theta));
+J = (1 / m) * sum(positiveClass - negativeClass) + ((lambda / (2 * m)) * (theta_N' * theta_N));
 
-J = (1 / m) * sum(positiveClass - negativeClass) + ((lambda / (2 * m)) * (thetaOneToN' * thetaOneToN));
+% Compute partial derivative for Theta 0
+x_Col_1 = X(:, 1);
+grad(1) = (1 / m) * x_Col_1' * (sigmoid(X * theta) - y);
 
-
-grad(:, 1) = (1 / m) * X(:, 1)' * (sigmoid(X * theta) - y);
-
-derivative_J = (1 / m) * X' * (sigmoid(X * theta) - y);
-regularizationFunc = (lambda / m) * thetaOneToN;
-
-grad = derivative_J + [grad(1); regularizationFunc];
+% Compute Theta 1 -> n
+x_j_PlusOne = X(: , 2:size(X, 2));
+grad(2:length(grad)) = ((1 / m) * x_j_PlusOne' * (sigmoid(X * theta) - y)) + ((lambda / m) * theta(2:length(theta)));
 
 % =============================================================
 
