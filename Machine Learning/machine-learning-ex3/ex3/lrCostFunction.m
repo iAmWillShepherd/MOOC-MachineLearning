5e58afa369b43df19cda19b126623d6e8d37dbed
William Shepherd <williamrshepherd@gmail.com>
2015-08-02 18:24:20 -0500

    Implemented regularized logistic regression cost and gradient function

--- Machine Learning/machine-learning-ex3/ex3/lrCostFunction.m
+++ Machine Learning/machine-learning-ex3/ex3/lrCostFunction.m
@@ -36,14 +36,22 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
+lengthOfTheta = length(theta);
+theta_0 = theta(1);
+theta_N = theta(2:length(theta));
 
+% Compute cost J
+positiveClass = -y' * log(sigmoid(X * theta));
+negativeClass = (1 - y)' * log(1 - sigmoid(X * theta));
+J = (1 / m) * sum(positiveClass - negativeClass) + ((lambda / (2 * m)) * (theta_N' * theta_N));
 
+% Compute partial derivative for Theta 0
+x_Col_1 = X(:, 1);
+grad(1) = (1 / m) * x_Col_1' * (sigmoid(X * theta) - y);
 
-
-
-
-
-
+% Compute Theta 1 -> n
+x_j_PlusOne = X(: , 2:size(X, 2));
+grad(2:length(grad)) = ((1 / m) * x_j_PlusOne' * (sigmoid(X * theta) - y)) + ((lambda / m) * theta(2:length(theta)));
 
 % =============================================================
 
