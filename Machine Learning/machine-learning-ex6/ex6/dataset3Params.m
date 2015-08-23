a87b2b3aaa4c8748e2ef67d7e2acd4799010c013
William Shepherd <williamrshepherd@gmail.com>
2015-08-22 23:19:38 -0500

    Implemented function that finds optimal C and theta params

--- Machine Learning/machine-learning-ex6/ex6/dataset3Params.m
+++ Machine Learning/machine-learning-ex6/ex6/dataset3Params.m
@@ -8,6 +8,7 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %
 
 % You need to return the following variables correctly.
+testValues = [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
 C = 1;
 sigma = 0.3;
 
@@ -22,11 +23,23 @@ sigma = 0.3;
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
 %
+prediction_error = 1;
 
-
-
-
-
+for i = 1:length(testValues)
+    for j = 1:length(testValues)
+        potential_C = testValues(i);
+        potential_sigma = testValues(j);
+        model= svmTrain(X, y, potential_C, @(x1, x2) gaussianKernel(x1, x2, potential_sigma));
+        predictions = svmPredict(model, Xval);
+        error = mean(double(predictions ~= yval));
+        
+        if (error < prediction_error)
+            C = potential_C; 
+            sigma = potential_sigma;
+            prediction_error = error;
+        end
+    end
+end
 
 
 % =========================================================================
